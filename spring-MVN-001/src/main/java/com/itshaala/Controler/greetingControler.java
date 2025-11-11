709c54b59408d52612236348f1026aa557cca61b
Vishnukant2003 <vishnukantgadekar2003@gmail.com>
2025-11-12 00:05:53 +0530

    spring-mvc basics

---
+++ spring-MVN-001/src/main/java/com/itshaala/Controler/greetingControler.java
@@ -0,0 +1,19 @@
+package com.itshaala.Controler;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
+
+import java.time.LocalDateTime;
+
+@Controller
+public class greetingControler {
+    @RequestMapping(value = "/greet", method = RequestMethod.GET)
+    public ModelAndView greet() {
+        ModelAndView mav = new ModelAndView();
+        mav.setViewName("greeting");
+        mav.addObject("message", "Good morning ! : " + LocalDateTime.now());
+        return mav;
+    }
+}
---
+++ spring-MVN-001/src/main/webapp/WEB-INF/views/greeting.jsp
@@ -0,0 +1,19 @@
+<%--
+  Created by IntelliJ IDEA.
+  User: ASUS
+  Date: 11-11-2025
+  Time: 23:30
+  To change this template use File | Settings | File Templates.
+--%>
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<html>
+<head>
+    <title>Title</title>
+</head>
+<body>
+<h1>hello greeting...</h1>
+
+<%=request.getAttribute("message")%>
+
+</body>
+</html>
